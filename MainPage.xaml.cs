cdf73741ad5566bfea480f53f75ddf8acbbcbe92
Armands Liepa <78017315+armands62@users.noreply.github.com>
2023-01-05 23:16:44 +0200

    DATU BĀZE SAVIENOTA....ķip
    Ja vēlaties palaist programmu, sākumā MainPage.xaml.cs pie connectionString jāievieto sava ip adrese uz kuras tiek hostota datubāze un viss pārējais info kas seko no tā.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿namespace ProStateApp2;
+using MySql.Data.MySqlClient;
 
 public partial class MainPage : ContentPage
 {
@@ -6,11 +7,35 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+    }
 
     private void Login_Clicked(object sender, EventArgs e)
     {
-		Application.Current.MainPage = new AppShell();
+        string connectionString = "server=192.168.1.198;user=armands;database=pro_state;password=qwerty;";
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string inputEmail = EpastsEntry.Text;
+            string inputPassword = ParoleEntry.Text;
+            using (var command = new MySqlCommand($"SELECT * FROM user WHERE email = \"{inputEmail}\"", connection))
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string password = reader.GetString(4);
+                    if (BCrypt.Net.BCrypt.Verify(inputPassword, password))
+                    {
+                        Application.Current.MainPage = new AppShell();
+                    }
+                    else
+                    {
+                      Error.IsVisible = true;
+                    }
+                }
+            }
+        }
     }
 }
 
